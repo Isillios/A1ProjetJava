113326a4e0abab38b8f0fe557f9a56dca75636b6
Axel Mola <axel.mola@viacesi.fr>
2018-06-05 20:40:37 +0200

    Last (?) push
    Last (?) push

--- Lorann/main/src/main/java/main/Main.java
+++ Lorann/main/src/main/java/main/Main.java
@@ -1,20 +1,35 @@
 package main;
 
-import java.io.IOException;
 import java.sql.SQLException;
-import model.Model;
-import view.View;
-import controller.Controller;
 
+import controller.ControllerFacade;
+import model.ModelFacade;
+import view.ViewFacade;
 
-public abstract class Main 
-{
-    public static void main(final String[] args) throws IOException, SQLException, InterruptedException
-    {
-    	final Model lorannmodel = new Model();
-    	final View lorannview = new View(lorannmodel.getMap());
-        final Controller loranncontroller = new Controller(lorannview, lorannmodel);
-        lorannview.setOrderPerformer(loranncontroller.getOrderPeformer());
-        loranncontroller.start();
+/**
+ * <h1>The Class Main.</h1>
+ *
+ * @author Jean-Aymeric DIET jadiet@cesi.fr
+ * @version 1.0
+ */
+public abstract class Main {
+
+    /**
+     * The main method.
+     *
+     * @param args
+     *            the arguments
+     */
+    public static void main(final String[] args) {
+        final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
+
+        try {
+            controller.start();
+        } catch (final SQLException exception) {
+            exception.printStackTrace();
+        }
     }
+
 }
+
+//test
